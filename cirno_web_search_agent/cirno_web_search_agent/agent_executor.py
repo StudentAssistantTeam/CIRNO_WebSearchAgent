1354a781f3740576fc75b0d2b55cf434c90420fa
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-22 14:17:19 +0800

    feat(a2a): Add agent executor

---
+++ cirno_web_search_agent/cirno_web_search_agent/agent_executor.py
@@ -0,0 +1,71 @@
+import logging
+# a2a dependencies
+from a2a.server.tasks import TaskUpdater
+from a2a.server.agent_execution import (
+    AgentExecutor,
+    RequestContext
+)
+from a2a.server.events import EventQueue
+from a2a.utils import (
+    new_task,
+    new_agent_text_message
+)
+from a2a.utils.errors import ServerError
+from a2a.types import (
+    InternalError,
+    TaskState,
+    Part,
+    TextPart
+)
+# Project dependencies
+from cirno_web_search_agent.agent import agent
+
+logger = logging.getLogger("agent executor")
+
+
+# Agent Executor
+class agent_executor(AgentExecutor):
+    def __init__(self):
+        self.agent = agent()
+
+    # Agent Execution
+    async def execute(
+            self,
+            context: RequestContext,
+            event_queue: EventQueue
+    ):
+        query = context.get_user_input()
+        task = context.current_task
+        if not task:
+            task = new_task(context.message)
+            await event_queue.enqueue_event(task)
+        # updater
+        updater = TaskUpdater(event_queue, task.id, task.context_id)
+        try:
+            # Agent initialization: Connect to the mcp
+            await self.agent.initialize()
+            # Streaming
+            async for chunk in self.agent.streaming(query, task.context_id):
+                # check whether it is done
+                is_done = chunk.done
+                if not is_done:
+                    # Updating task
+                    await updater.update_status(
+                        TaskState.working,
+                        new_agent_text_message(
+                            chunk.content,
+                            context_id=task.context_id,
+                            task_id=task.id
+                        )
+                    )
+                else:
+                    await updater.add_artifact(
+                        [Part(root=TextPart(text=chunk.content))],
+                        name="conversation_result"
+                    )
+                    await updater.complete()
+                    break
+        except Exception as e:
+            # Error handling
+            logger.error(f"Agent execution failed due to {e}")
+            raise ServerError(error=InternalError()) from e
